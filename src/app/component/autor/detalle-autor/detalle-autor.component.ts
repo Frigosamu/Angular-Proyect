74795567b3b8986475318ccb044bdd3db41cba3c
Frigosamu <Frigosamu@gmail.com>
2025-02-26 13:37:10 +0100

    Mapa en el detalle de autor terminado

--- src/app/component/autor/detalle-autor/detalle-autor.component.ts
+++ src/app/component/autor/detalle-autor/detalle-autor.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { Autor } from '../../../model/autor';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AutorService } from '../../../services/autor.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Map, tileLayer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-detalle-autor',
@@ -11,32 +12,32 @@ import { CommonModule } from '@angular/common';
   templateUrl: './detalle-autor.component.html',
   styleUrl: './detalle-autor.component.css'
 })
-export class DetalleAutorComponent {
-  autores: Autor[] = [];
-  autorId: string = '';
+export class DetalleAutorComponent implements AfterViewInit {
   form: FormGroup;
+  autorId: string = '';
+  map!: Map;
+  latitud!: number;
+  longitud!: number;
 
   constructor(
     private autorService: AutorService,
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private router: Router
-
   ) {
-    this.autorService.getAutores().subscribe(a => this.autores = a);
-
-    const routeParams = this.route.snapshot.paramMap;
-    this.autorId = routeParams.get('id')!;
+    this.autorId = this.route.snapshot.paramMap.get('id')!;
 
     this.form = this.fb.group({
-      id: ['', [Validators.required]],
-      nombre: ['', [Validators.required]],
-      anioNacimiento: ['', [Validators.required]],
-      latitud: ['', [Validators.required]],
-      longitud: ['', [Validators.required]],
+      id: ['', Validators.required],
+      nombre: ['', Validators.required],
+      anioNacimiento: ['', Validators.required],
+      latitud: ['', Validators.required],
+      longitud: ['', Validators.required],
     });
+  }
 
-    this.autorService.getAutor(this.autorId!).subscribe(autorDetalle => {
+  ngAfterViewInit(): void {
+    this.autorService.getAutor(this.autorId).subscribe(autorDetalle => {
       this.form.patchValue({
         id: autorDetalle.id,
         nombre: autorDetalle.nombre,
@@ -45,6 +46,25 @@ export class DetalleAutorComponent {
         longitud: autorDetalle.lugarNacimiento?.longitud
       });
 
+      this.latitud = autorDetalle.lugarNacimiento?.latitud;
+      this.longitud = autorDetalle.lugarNacimiento?.longitud;
+
+      this.initMap();
     });
+  }
+
+  private initMap() {
+    if (!this.latitud || !this.longitud) return;
+
+    this.map = new Map('map').setView([this.latitud, this.longitud], 13);
+
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    marker([this.latitud, this.longitud]).addTo(this.map)
+      .bindPopup('Lugar de nacimiento')
+      .openPopup();
   }
 }
