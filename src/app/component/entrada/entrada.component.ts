86b3d93a9ca9f9ca151bb79a91cc5fe708844fda
Frigosamu <Frigosamu@gmail.com>
2025-02-26 14:34:38 +0100

    Terminado.

--- src/app/component/entrada/entrada.component.ts
+++ src/app/component/entrada/entrada.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+import { Libro } from '../../model/libro';
+import { LibroService } from '../../services/libro.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-entrada',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './entrada.component.html',
   styleUrl: './entrada.component.css'
 })
 export class EntradaComponent {
+  libros: Libro[] = [];
 
+  constructor(private libroService: LibroService) {}
+
+  ngOnInit() {
+    this.libroService.getLibros().subscribe(libros => {
+      this.libros = libros;
+    });
+  }
 }
